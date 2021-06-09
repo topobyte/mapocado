3334de06a3648bb9690a057a3d0d949152ea27e2
Sebastian Kürten <sebastian@topobyte.de>
2021-06-09 10:36:29 +0200

    Add license headers to source files

--- core/src/main/java/de/topobyte/mapocado/styles/misc/PrimitiveParser.java
+++ core/src/main/java/de/topobyte/mapocado/styles/misc/PrimitiveParser.java
@@ -1,3 +1,20 @@
+// Copyright 2021 Sebastian Kuerten
+//
+// This file is part of mapocado.
+//
+// mapocado is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mapocado is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with mapocado. If not, see <http://www.gnu.org/licenses/>.
+
 package de.topobyte.mapocado.styles.misc;
 
 public class PrimitiveParser

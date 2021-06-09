4a9c9715a22bb2a4ca9fdf2f3c04591de2e34649
Sebastian Kürten <sebastian@topobyte.de>
2021-06-09 10:36:03 +0200

    Remove a bunch of osm4j headers

--- tools/core/src/main/java/de/topobyte/jeography/viewer/nomioc/ActivatableJList.java
+++ tools/core/src/main/java/de/topobyte/jeography/viewer/nomioc/ActivatableJList.java
@@ -1,22 +1,3 @@
-/* Osm4j is a library that provides utilities to handle Openstreetmap data in java.
- *
- * Copyright (C) 2011  Sebastian Kuerten
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
 package de.topobyte.jeography.viewer.nomioc;
 
 import java.awt.event.ActionEvent;
